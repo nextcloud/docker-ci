1022dcedf02ffea3fd75e20a508d24b135f5b7b8
Morris Jobke <hey@morrisjobke.de>
2016-09-05 14:59:03 +0200

    add script for the appstore

---
+++ translations/handleAppstoreTranslations.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# verbose and exit on error
+set -xe
+
+# import GPG keys
+gpg --import /gpg/nextcloud-bot.public.asc
+gpg --allow-secret-key-import --import /gpg/nextcloud-bot.asc
+gpg --list-keys
+
+# fetch git repo
+git clone git@github.com:nextcloud/appstore /app
+
+# install django
+pip install Django==1.9.8
+
+# create po files
+./manage.py makemessages
+
+# push sources
+tx push -s
+
+# pull translations - force pull because a fresh clone has newer time stamps
+tx pull -f -a --minimum-perc=75
+
+# create git commit and push it
+git add locale/
+git checkout test-it
+git commit -am "[tx-robot] updated from transifex" || true
+git push origin test-it
+echo "done"
