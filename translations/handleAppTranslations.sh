d3f86cfc1c5c54f55c995abf28a70255af67abc8
Morris Jobke <hey@morrisjobke.de>
2017-06-03 09:35:01 +0200

    Also backport translations for all apps
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

--- translations/handleAppTranslations.sh
+++ translations/handleAppTranslations.sh
@@ -17,6 +17,41 @@ wget https://raw.githubusercontent.com/nextcloud/docker-ci/master/translations/l
 wget https://raw.githubusercontent.com/nextcloud/server/master/build/l10nParseAppInfo.php
 perl ./l10n.pl $1 read
 
+versions='stable11 stable12 master abc'
+
+# build POT files for all versions
+mkdir stable-templates
+for version in $versions
+do
+  # skip if the branch doesn't exist
+  if git branch | egrep "^  $version$" ; then
+    echo "Valid branch"
+  else
+    echo "Invalid branch"
+    continue
+  fi
+  git checkout $version
+  perl ./l10n.pl $1 read > /dev/null
+  cd templates
+  for file in $(ls)
+  do
+    mv $file ../stable-templates/$version.$file
+  done
+  cd ..
+done
+
+# merge POT files into one
+for file in $(ls stable-templates/master.*)
+do
+  name=$(echo $file | cut -b 25- )
+  msgcat --use-first stable-templates/*.$name > templates/$name
+done
+
+rm l10nParseAppInfo.php
+
+# remove intermediate POT files
+rm -rf stable-templates
+
 # push sources
 tx push -s
 
@@ -26,21 +61,39 @@ tx pull -f -a --minimum-perc=25
 # delete removed l10n files that are used for language detection (they will be recreated during the write)
 rm -f *.js *.json
 
-# build JS/JSON based on translations
-perl ./l10n.pl $1 write
-
-rm l10n.pl
-rm l10nParseAppInfo.php
 cd ..
 
-if [ -d tests ]; then
-  # remove tests/
-  rm -rf tests
-  git checkout -- tests/
-fi
+backportVersions='master stable12 stable11'
+for version in $backportVersions
+do
+  # skip if the branch doesn't exist
+  if git branch | egrep "^  $version$" ; then
+    echo "Valid branch"
+  else
+    echo "Invalid branch"
+    continue
+  fi
+  git checkout $version
 
-# create git commit and push it
-git add l10n/*.js l10n/*.json
-git commit -am "[tx-robot] updated from transifex" || true
-git push origin master
-echo "done"
+  cd l10n
+
+  # build JS/JSON based on translations
+  perl ./l10n.pl $1 write
+
+  cd ..
+
+  if [ -d tests ]; then
+    # remove tests/
+    rm -rf tests
+    git checkout -- tests/
+  fi
+
+  # create git commit and push it
+  git add l10n/*.js l10n/*.json
+  git commit -am "[tx-robot] updated from transifex" || true
+  git push origin $version
+
+  echo "done with $version"
+done
+
+rm l10n.pl
