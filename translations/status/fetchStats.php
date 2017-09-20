778f83b44876c9ada168fc998823475b7447b61c
Morris Jobke <hey@morrisjobke.de>
2017-09-20 12:49:33 +0200

    Add little script to show untriaged issues
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

---
+++ translations/status/fetchStats.php
@@ -0,0 +1,34 @@
+<?php
+
+$filename = __DIR__ . '/stats';
+$url = 'https://github.com/nextcloud/server/issues?q=is%3Aissue%20is%3Aopen%20-label%3Aenhancement%20-label%3Aspec%20-label%3Asecurity%20-label%3Abug%20-label%3Apapercut%20-label%3Aoverview%20%20-label%3A%22technical%20debt%22%20';
+
+$body = file_get_contents($url);
+
+$body = str_replace("\n", "", $body);
+
+$pattern = '!<div class="table-list-filters".*</a>!U';
+
+preg_match_all($pattern, $body, $matches);
+
+if (!isset($matches[0][0])) {
+    die('Could not find element.');
+}
+
+$result = preg_replace('!<.*>!U', '', $matches[0][0]);
+
+preg_match('!(\d+) open!i', $result, $matches);
+
+if (!isset($matches[1])) {
+    die('Could not find result set.');
+}
+$number = $matches[1];
+
+$date = (new DateTime())->format('Y-m-d');
+
+$fileContent = file_get_contents($filename);
+
+if (strpos($fileContent, $date . ' ') === false) {
+    $fileContent .= PHP_EOL . $date . ' ' . $number;
+    file_put_contents($filename, $fileContent);
+}
---
+++ translations/status/issues.php
@@ -0,0 +1,78 @@
+<?php
+
+$filename = __DIR__ . '/stats';
+
+$lines = explode(PHP_EOL, file_get_contents($filename));
+
+$elements = array_map(function($line) {
+    return explode(' ', $line);
+}, $lines);
+
+?>
+<html lang="de">
+    <head>
+        <meta charset="utf-8">
+        <title>Untriaged issues</title>
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
+        <style>
+            body {
+                font-family: sans-serif;
+            }
+            .page {
+                max-width: 800px;
+                margin: 0 auto;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="page">
+            <h1>Untriaged issues</h1>
+            <p>The total number of untriaged issues needs to be close to 0. <a href="https://github.com/nextcloud/server/issues?utf8=✓&amp;q=is%3Aissue%20is%3Aopen%20-label%3Aenhancement%20-label%3Aspec%20-label%3Asecurity%20-label%3Abug%20-label%3Apapercut%20-label%3Aoverview%20%20-label%3A%22technical%20debt%22%20">Triage issues on Github.</a></p>
+            <canvas id="myChart"></canvas>
+        </div>
+        <script>
+            var ctx = document.getElementById('myChart').getContext('2d');
+            var chart = new Chart(ctx, {
+                // The type of chart we want to create
+                type: 'line',
+
+                // The data for our dataset
+                data: {
+                    datasets: [{
+                        label: "Untriaged issues",
+                        fill: false,
+                        borderColor: 'rgb(255, 99, 132)',
+                        data: [
+                            <?php
+                            foreach($elements as $element) {
+                                vprintf("{x: new Date('%s'), y: %s},\n", $element);
+                            }
+                            ?>
+                        ],
+                    }]
+                },
+
+                // Configuration options go here
+                options: {
+                    scales: {
+                        xAxes: [{
+                            type: 'time',
+                            time: {
+                                unit: 'day'
+                            }
+                        }],
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true,
+                            },
+                        }],
+                    }
+                }
+            });
+        </script>
+    </body>
+</html>
