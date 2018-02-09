6c61653dd460d7c13d5284a5c1a15a7064f11bb3
Morris Jobke <hey@morrisjobke.de>
2018-02-09 16:02:11 +0100

    Unperl the server l10n script
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

--- translations/handleTranslations.sh
+++ translations/handleTranslations.sh
@@ -11,28 +11,32 @@ gpg --list-keys
 # fetch git repo
 git clone git@github.com:nextcloud/server /app
 
+# fetch translation script
+# TODO ship this inside the docker container
+wget https://github.com/nextcloud/travis_ci/raw/master/translationtool/translationtool.phar
+
 versions='stable12 stable13 master'
 
 # build POT files for all versions
-cd l10n
 mkdir stable-templates
 for version in $versions
 do
   git checkout $version
-  perl ./l10n.pl read > /dev/null
-  cd templates
+  php5 translationtool.phar create-pot-files
+
+  cd translationfiles/templates/
   for file in $(ls)
   do
-    mv $file ../stable-templates/$version.$file
+    mv $file ../../stable-templates/$version.$file
   done
-  cd ..
+  cd ../..
 done
 
 # merge POT files into one
 for file in $(ls stable-templates/master.*)
 do
   name=$(echo $file | cut -b 25- )
-  msgcat --use-first stable-templates/*.$name > templates/$name
+  msgcat --use-first stable-templates/*.$name > translationfiles/templates/$name
 done
 
 # remove intermediate POT files
@@ -43,10 +47,8 @@ tx push -s
 
 # pull translations - force pull because a fresh clone has newer time stamps
 tx pull -f -a --minimum-perc=75
-# pull all settings for language name
-tx pull -a -f -r nextcloud.settings-1 --minimum-perc=1
-
-cd ..
+# pull all "lib" translations for the language name
+tx pull -a -f -r nextcloud.lib --minimum-perc=1
 
 backportVersions='master stable13 stable12'
 for version in $backportVersions
@@ -58,19 +60,15 @@ do
   find lib/l10n -type f -delete
   find settings/l10n -type f -delete
 
-  cd l10n
-
   # build JS/JSON based on translations
-  perl ./l10n.pl write
-
-  cd ..
+  php5 translationtool.phar convert-po-files
 
   # remove tests/
   rm -rf tests
   git checkout -- tests/
 
   # create git commit and push it
-  git add .
+  git add apps core lib settings
   git commit -am "[tx-robot] updated from transifex" || true
   git push origin $version
 
