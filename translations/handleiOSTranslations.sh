89545bc0145740a0448639898ee59c009b4a41df
Morris Jobke <hey@morrisjobke.de>
2018-10-16 18:27:04 +0200

    Update iOS translation container to new approach
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

--- translations/handleiOSTranslations.sh
+++ translations/handleiOSTranslations.sh
@@ -25,17 +25,6 @@ tx pull -f -a --minimum-perc=25
 
 cd Supporting\ Files
 
-# remove folder that doesn't contain all translations
-l10nFolder=$(ls -1 | grep lproj)
-
-for folder in $l10nFolder; do
-    count=$(ls -1 $folder | wc -l)
-    if [ $count != "7" ]; then
-        echo remove $folder
-        rm -rf ./$folder
-    fi
-done
-
 # use de_DE instead of de
 rm -rf ./de.lproj
 mv de_DE.lproj de.lproj
