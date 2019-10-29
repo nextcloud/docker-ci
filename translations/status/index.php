27663ebf46bfd08e2f1eae3cdce0ba89901e6dca
tobiasKaminsky <tobias@kaminsky.me>
2019-10-29 08:29:54 +0100

    output 20 instead of 10 lines
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- translations/status/index.php
+++ translations/status/index.php
@@ -79,7 +79,7 @@ foreach ($files as $file) {
 	}
 
 	$info = [
-        'logLines' => str_replace(['"', '\''], ['\"', '\\\''], htmlentities(implode('\n', array_slice($logLines, max(sizeof($logLines) - 10, 0))))),
+        'logLines' => str_replace(['"', '\''], ['\"', '\\\''], htmlentities(implode('\n', array_slice($logLines, max(sizeof($logLines) - 20, 0))))),
         'status' => $result->errorMessage === '' ? 'success' : 'error',
         'duration' => $diff->format($format),
         'start' => $result->startDate->format('Y-m-d H:i:s'),
