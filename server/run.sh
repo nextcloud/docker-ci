9837c48d8127c7786934ad5c53fa20f3e8e588f3
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2019-11-05 21:08:31 +0100

    Cleanup, remove fpm and use original apache foreground exec
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- server/run.sh
+++ server/run.sh
@@ -5,6 +5,10 @@ cd /var/www/html/
 
 . /etc/apache2/envvars
 
+# allow php and apache2 to create their run socket
+mkdir -p /run/php
+mkdir -p /var/run/apache2
+
 tail -f data/nextcloud.log &
 
-apache2-foreground "$@"
+apache2 -DFOREGROUND "$@"
