a89bd220d21c4419f92532de5a2fea2c2c995351
Michael Schuster <michael@schuster.ms>
2020-02-19 03:19:36 +0100

    Allow Desktop translation merging and custom backport branches
    Merge translation source files for master and stable branches to avoid losing translations.
    
    For reference, this commit has been influenced by:
    - https://github.com/nextcloud/docker-ci/blob/master/translations/handlePlainTranslations.sh
    - https://github.com/nextcloud/docker-ci/pull/165
    - https://stackoverflow.com/a/8527556
    
    Signed-off-by: Michael Schuster <michael@schuster.ms>

--- translations/handleDesktopTranslations.sh
+++ translations/handleDesktopTranslations.sh
@@ -12,16 +12,58 @@ gpg --list-keys
 git clone git@github.com:nextcloud/desktop.git
 cd desktop
 
-lupdate-qt5 src/gui/ src/cmd/ src/common/ src/crashreporter/ src/csync/ src/libsync/ -ts translations/client_en.ts
+# Generate source translation files for master and stable branches
+mkdir /branches
+
+versions="stable-2.6 master"
+if [[ -f '.tx/backport' ]]; then
+  versions="$(cat .tx/backport) master"
+fi
+
+for version in $versions
+do
+  # skip if the branch doesn't exist
+  if git branch -r | egrep "^\W*origin/$version$" ; then
+    echo "Valid branch"
+  else
+    echo "Invalid branch"
+    continue
+  fi
+
+  git checkout $version
+
+  lupdate-qt5 src/gui/ src/cmd/ src/common/ src/crashreporter/ src/csync/ src/libsync/ -ts /branches/$version.ts
+done
+
+# Merge source translation files and filter duplicates
+lconvert-qt5 -i /branches/*.ts -o translations/client_en.ts
 
 # push sources
 tx push -s
 
-# pull translations
-tx pull -f -a --minimum-perc=25
+# undo local changes
+git checkout -f --
 
-# create git commit and push it
-git add .
-git commit -am "[tx-robot] updated from transifex" || true
-git push origin master
-echo "done"
+# reverse version list to apply backports
+backportVersions=$(echo $versions | awk '{for(i=NF;i>=1;i--) printf "%s ", $i;print ""}')
+for version in $backportVersions
+do
+  # skip if the branch doesn't exist
+  if git branch -r | egrep "^\W*origin/$version$" ; then
+    echo "Valid branch"
+  else
+    echo "Invalid branch"
+    continue
+  fi
+
+  git checkout $version
+
+  # pull translations
+  tx pull -f -a --minimum-perc=25
+
+  # create git commit and push it
+  git add .
+  git commit -am "[tx-robot] updated from transifex" || true
+  git push origin $version
+  echo "done with $version"
+done
