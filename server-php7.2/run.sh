9c579920db6f02d7d93a1a942ceedd3c155cf0d2
tobiasKaminsky <tobias@kaminsky.me>
2022-03-14 15:33:43 +0100

    Add CI server for 7.2 to test against NC16
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

---
+++ server-php7.2/run.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+cd /var/www/html/
+
+. /etc/apache2/envvars
+
+# allow php and apache2 to create their run socket
+mkdir -p /run/php
+mkdir -p /var/run/apache2
+
+tail -f data/nextcloud.log &
+
+a2enmod ssl
+a2enmod headers
+a2ensite default-ssl
+a2enconf ssl-params
+apache2ctl configtest
+
+apache2 -DFOREGROUND "$@"
