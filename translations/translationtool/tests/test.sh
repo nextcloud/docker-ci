a8251dfe8b9b28acee71dd0ecaf55c26f4c62338
Ferdinand Thiessen <opensource@fthiessen.de>
2025-04-04 12:47:55 +0200

    test: adjust test script to not remove splitted translations
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- translations/translationtool/tests/test.sh
+++ translations/translationtool/tests/test.sh
@@ -2,8 +2,11 @@
 php ../src/translationtool.php create-pot-files
 
 # Remove unstable data like "POT-Creation-Date: 2024-09-10 14:25+0200\n"
-# And remove the development path
-cat translationfiles/templates/tests.pot | grep -v '^"' | sed -e "s/${PWD//\//\\/}\\///" > expected.pot
+cat translationfiles/templates/tests.pot | \
+	# Remove meta data
+	tail -n +21 | \
+	# And remove the development path
+	sed -e "s/${PWD//\//\\/}\\///" > expected.pot
 
 CHANGED_LINES=$(git diff expected.pot | wc -l)
 
