5ed96fa6f9535415f9da1e672f38d7f7c659b2ee
Joas Schilling <coding@schilljs.com>
2025-06-30 07:03:21 +0200

    chore: Adjust copyright
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- translations/translationtool/src/translationtool.php
+++ translations/translationtool/src/translationtool.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /**
- * SPDX-FileCopyrightText: 2017, 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-FileCopyrightText: 2017-2025 Nextcloud GmbH and Nextcloud contributors
  * SPDX-FileCopyrightText: 2017 Jakob Sack <nextcloud@jakobsack.de>
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
