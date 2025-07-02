e27a248c356c0542dc1c214a772e429b15ea27d1
Joas Schilling <coding@schilljs.com>
2025-07-02 16:46:34 +0200

    fix(translation): Fix resource list of iOS Notes
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- translations/handleiOSNotesTranslations.sh
+++ translations/handleiOSNotesTranslations.sh
@@ -19,13 +19,11 @@ rm -r Source/*.lproj/Localizable.strings
 rm -r Source/*.lproj/Main_iPhone.strings
 rm -r Source/*.lproj/Categories.strings
 rm -r Source/Settings.bundle/*.lproj/Root.strings
-rm -r Source/Screens/Settings/*.lproj/Settings.strings
 
 git checkout -- Source/en.lproj/Localizable.strings
 git checkout -- Source/en.lproj/Main_iPhone.strings
 git checkout -- Source/en.lproj/Categories.strings
 git checkout -- Source/Settings.bundle/en.lproj/Root.strings
-git checkout -- Source/Screens/Settings/en.lproj/Settings.strings
 
 
 # push sources
@@ -40,13 +38,11 @@ rm -rf Source/de.lproj/Localizable.strings
 rm -rf Source/de.lproj/Main_iPhone.strings
 rm -rf Source/de.lproj/Categories.strings
 rm -rf Source/Settings.bundle/de.lproj/Root.strings
-rm -rf Source/Screens/Settings/de.lproj/Settings.strings
 
 mv Source/de_DE.lproj/Localizable.strings Source/de.lproj/Localizable.strings
 mv Source/de_DE.lproj/Main_iPhone.strings Source/de.lproj/Main_iPhone.strings
 mv Source/de_DE.lproj/Categories.strings Source/de.lproj/Categories.strings
 mv Source/Settings.bundle/de_DE.lproj/Root.strings Source/Settings.bundle/de.lproj/Root.strings
-mv Source/Screens/Settings/de_DE.lproj/Settings.strings Source/Screens/Settings/de.lproj/Settings.strings
 
 # create git commit and push it
 git add .
