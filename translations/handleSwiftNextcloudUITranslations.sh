ada387ff9e7b56033054cebecbb16713c145b16b
Joas Schilling <coding@schilljs.com>
2025-08-01 13:42:07 +0200

    fix(SwiftNextcloudUI): Correctly map de_DE to de version again
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- translations/handleSwiftNextcloudUITranslations.sh
+++ translations/handleSwiftNextcloudUITranslations.sh
@@ -20,6 +20,12 @@ tx push -s
 # pull translations
 tx pull -f -a
 
+# Use de_DE instead of de
+# 1. Remove informal german keys
+# 2. Replace formal german with informal german key
+# 3. Save again
+cat Sources/SwiftNextcloudUI/Localizable.xcstrings | jq '. | del(.strings .[] .localizations .de)' | sed 's/"de_DE": {/"de": {/' > Sources/SwiftNextcloudUI/Localizable.xcstrings
+
 # create git commit and push it
 git add .
 git commit -am "fix(l10n): Update translations from Transifex" -s || true
